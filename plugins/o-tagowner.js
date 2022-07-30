64958e059793a3daa318511c771a89beaba2f001
Hm <110269965+FachriBotV4@users.noreply.github.com>
2022-07-30 11:57:39 +0700

    Update o-tagowner.js

--- plugins/o-tagowner.js
+++ plugins/o-tagowner.js
@@ -1,15 +1,13 @@
 let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *Ayaang ada yg manggil tuh, mau sewa mungkin:v*`, m)
+conn.reply(global.owner[0] + `@s.whatsapp.net`, ` *Ayaang ada yg manggil tuh:v*`, m)
 
   conn.reply(m.chat, `
-Ngapain lu manggil owner ku dek?
-kalau mau sewa chat pribadi aja
-Gak usah Malu-malu
+Ngapain lu manggil owner ku kak?
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@66855048169 |@66855048169/i
+handler.customPrefix = /@6285713041886/i
 handler.command = new RegExp
 
 module.exports = handler
